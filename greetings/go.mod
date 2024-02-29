f19fcb96c6b602d8db3444f9b62faf0ef19ace0b
Coding-jpg <1305856002@qq.com>
2024-02-29 19:25:44 +1100

    update

---
+++ greetings/go.mod
@@ -0,0 +1,3 @@
+module Let-s-GO/greetings
+
+go 1.22.0
---
+++ greetings/greetings.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func Hello(name string) string {
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
+}
