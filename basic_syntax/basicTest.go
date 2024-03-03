dc9f32c8c0dfcc537845fdc70e253e9fd8e9eaf0
Coding-jpg <1305856002@qq.com>
2024-03-03 11:46:51 +1100

    update

---
+++ basic_syntax/basicTest.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	// x := "test"
+	test_lst := []int{}
+	for i, v := range 3 {
+		test_lst = append(test_lst, v)
+	}
+	fmt.print(x)
+	// if x == "test" {
+	// 	fmt.Printf("this is a %s\n", x)
+	// }
+
+}
---
+++ basic_syntax/go.mod
@@ -0,0 +1,3 @@
+module Let-s-GO/basic
+
+go 1.22.0
